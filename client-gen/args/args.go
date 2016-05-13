kubernetes/kubernetes

Updating client-gen to support a user specified override on the resources to be included in generated client

cmd/libs/go2idl/client-gen/args/args.go
@@ -29,6 +29,10 @@ type Args struct {
 	// the respective types.go. We still need GroupVersions in the struct because
 	// we need an order.
 	GroupVersionToInputPath map[unversioned.GroupVersion]string
+
+	// Overrides for which resources should be included in the client.
+	IncludeResourceOverrides map[unversioned.GroupVersion][]string
+
 	// ClientsetName is the name of the clientset to be generated. It's
 	// populated from command-line arguments.
 	ClientsetName string

cmd/libs/go2idl/client-gen/generators/client_generator.go
@@ -166,15 +166,32 @@ func Packages(context *generator.Context, arguments *args.GeneratorArgs) generat
 	if !ok {
 		glog.Fatalf("cannot convert arguments.CustomArgs to clientgenargs.Args")
 	}
+	includeResourceOverrides := customArgs.IncludeResourceOverrides
 
 	generatedBy := generatedBy(customArgs)
 
 	gvToTypes := map[unversioned.GroupVersion][]*types.Type{}
 	for gv, inputDir := range customArgs.GroupVersionToInputPath {
 		p := context.Universe.Package(inputDir)
-		for _, t := range p.Types {
-			if types.ExtractCommentTags("+", t.SecondClosestCommentLines)["genclient"] != "true" {
-				continue
+		for n, t := range p.Types {
+			// filter out resources which are not included in user specified override.
+			resources, ok := includeResourceOverrides[gv]
+			if ok {
+				found := false
+				for _, resource := range resources {
+					if resource == n {
+						found = true
+					}
+				}
+				if !found {
+					continue
+				}
+			} else {
+				// User has not specified any override for this group version.
+				// filter out types which dont have genclient=true.
+				if types.ExtractCommentTags("+", t.SecondClosestCommentLines)["genclient"] != "true" {
+					continue
+				}
 			}
 			if _, found := gvToTypes[gv]; !found {
 				gvToTypes[gv] = []*types.Type{}

Serabe/go2idl

client-gen/args/args.go
@@ -29,6 +29,10 @@ type Args struct {
 	// the respective types.go. We still need GroupVersions in the struct because
 	// we need an order.
 	GroupVersionToInputPath map[unversioned.GroupVersion]string
+
+	// Overrides for which types should be included in the client.
+	IncludedTypesOverrides map[unversioned.GroupVersion][]string
+
 	// ClientsetName is the name of the clientset to be generated. It's
 	// populated from command-line arguments.
 	ClientsetName string

client-gen/generators/client_generator.go
@@ -166,15 +166,32 @@ func Packages(context *generator.Context, arguments *args.GeneratorArgs) generat
 	if !ok {
 		glog.Fatalf("cannot convert arguments.CustomArgs to clientgenargs.Args")
 	}
+	includedTypesOverrides := customArgs.IncludedTypesOverrides
 
 	generatedBy := generatedBy(customArgs)
 
 	gvToTypes := map[unversioned.GroupVersion][]*types.Type{}
 	for gv, inputDir := range customArgs.GroupVersionToInputPath {
 		p := context.Universe.Package(inputDir)
-		for _, t := range p.Types {
-			if types.ExtractCommentTags("+", t.SecondClosestCommentLines)["genclient"] != "true" {
-				continue
+		for n, t := range p.Types {
+			// filter out types which are not included in user specified overrides.
+			typesOverride, ok := includedTypesOverrides[gv]
+			if ok {
+				found := false
+				for _, typeStr := range typesOverride {
+					if typeStr == n {
+						found = true
+					}
+				}
+				if !found {
+					continue
+				}
+			} else {
+				// User has not specified any override for this group version.
+				// filter out types which dont have genclient=true.
+				if types.ExtractCommentTags("+", t.SecondClosestCommentLines)["genclient"] != "true" {
+					continue
+				}
 			}
 			if _, found := gvToTypes[gv]; !found {
 				gvToTypes[gv] = []*types.Type{}